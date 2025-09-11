3d10024fa850e88b72d6e17a233a363921331ab5
seungjjun <stw550@gmail.com>
2025-09-11 19:32:45 +0900

    Adds solution for SearchInsertPosition (O(log n))

---
+++ leetcode/week2/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+                continue;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
