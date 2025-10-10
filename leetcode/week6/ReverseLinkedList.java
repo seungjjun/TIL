cd221445e5869965116811475cad6796705c14a8
seungjjun <stw550@gmail.com>
2025-10-10 20:58:46 +0900

    Adds solution for ReverseLinkedList (O(n))

---
+++ leetcode/week6/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+package com.seungjjun.composition;
+
+public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
