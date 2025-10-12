24a21e964a93df08de724d185d6441ff6538b00f
seungjjun <stw550@gmail.com>
2025-10-12 15:53:23 +0900

    Adds solution for AsteroidCollision (O(n))

---
+++ leetcode/week6/AsteroidCollision.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class AsteroidCollision {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            if (asteroid > 0) {
+                stack.push(asteroid);
+                continue;
+            }
+
+            while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid)) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty() || stack.peek() < 0) {
+                stack.push(asteroid);
+                continue;
+            }
+            if (stack.peek() == Math.abs(asteroid)) {
+                stack.pop();
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
